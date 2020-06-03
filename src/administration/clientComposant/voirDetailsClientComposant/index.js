a4f169abac927e8915f624be796f18cd46f19fe7
lePlusPhydel <fieni2fidel@gmail.com>
2020-06-03 20:29:42 +0100

    boutons supprimer

---
+++ src/administration/clientComposant/voirDetailsClientComposant/index.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react';
+import {style} from './index.scss';
+
+class VoirDetailsClientComposant extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className={style}>
+                
+            </div>
+        );
+    }
+}
+
+VoirDetailsClientComposant.propTypes = {
+
+};
+
+export default VoirDetailsClientComposant;
